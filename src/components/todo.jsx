c7519afc54df7684b5627588eea9d1127ce474d8
Jhack.Costa <jhack.oliveira@singlepoint.com.br>
2023-08-19 11:06:49 -0300

    Adicionando e removendo tarefas

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,16 +2,16 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-const Todo = ({todo}) => {
+const Todo = ({todo,removeTodo,completeTodo}) => {
     return(
-        <div className='todo'>
+        <div className='todo' style={{textDecoration: todo.isComplete ? "line-through": ""}}>
             <div className='content'>
               <p>{todo.text}</p>
               <p className='category'>({todo.category})</p>
             </div>
             <div>
-              <button className="complete">Completar</button>
-              <button className="remove">Excluir</button>
+              <button className="complete" onClick={() =>completeTodo(todo.id)}>Completar</button>
+              <button className="remove" onClick={() => removeTodo(todo.id)}>Excluir</button>
             </div>
           </div>
     )
